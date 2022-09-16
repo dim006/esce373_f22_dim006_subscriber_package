9e7ce59a4a0180554623e6b3d7cef3096de167cb
dim6 <dim6@case.edu>
2022-09-16 19:02:46 -0400

    Update Subscriber Repo #1

---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/diego/catkin_ws/src/subscriber_package/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/diego/catkin_ws/src/subscriber_package/catkin_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/diego/catkin_ws/src/subscriber_package/catkin_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/diego/catkin_ws/src/subscriber_package/catkin_ws/src:$ROS_PACKAGE_PATH"
